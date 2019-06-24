ca933cc207796d6136b2917f1332d8cb31db3bdb
CN0107 <cn0107@CN0107.lan>
2019-06-24 17:30:32 +0100

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    contacts: [
+      {
+        firstName: 'Max', 
+        lastName: 'Riley', 
+        contactNumber: 12345678901
+      }
+    ]
+  }
+
+  addContact = (e) => {
+    e.preventDefault()
+    let list = this.state.contacts
+    list.push({ 
+      firstName: e.target[0].value, 
+      lastName: e.target[1].value, 
+      contactNumber: e.target[2].value
+    })
+
+    console.log(list)
+    
+    this.setState({contacts: list})
+    console.log(this.state)
+  }
+
+  render(){
+
+   return (
+   <header className="App-header">
+      <div className="container">
+       <h2>New Contact</h2>
+        <form onSubmit={e => this.addContact(e)}>
+          <input placeholder="First Name" className="firstName"/>
+          <input placeholder="Last Name" className="lastName"/>
+          <input placeholder="Contact Number" className="contactNumber"/>
+          <button type="submit" className="button">Add All</button>
+        </form>
+        <div className="contacts">
+          {
+            this.state.contacts.map(contact => <NewContact contact={contact}/>)
+          }
+        </div>
+      </div>
+    </header>
+    );
+  }
 }
+
+const NewContact = ({ contact }) => {
+  return(
+    <div className="contactContainer">
+      <div className="name">
+       {`${contact.firstName} ${contact.lastName}`}
+      </div>
+      <div className="number">
+        {contact.contactNumber}
+      </div>
+    </div>
+  )
+}
+
 
 export default App;
